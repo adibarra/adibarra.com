01d264b90bfe9ce0b20995eb415ff0564c520b61
adibarra <93070681+adibarra@users.noreply.github.com>
2022-10-21 20:17:14 -0500

    fix broken email

--- scripts/rss.ts
+++ scripts/rss.ts
@@ -9,7 +9,7 @@ import { Feed } from 'feed'
 const DOMAIN = 'https://adibarra.com'
 const AUTHOR = {
   name: 'Alec Ibarra',
-  email: 'hi@adibara.com',
+  email: 'hi@adibarra.com',
   link: DOMAIN,
 }
 const markdown = MarkdownIt({
