4140bd13ca23179f7a08af2b52df8d2375886203
adibarra <93070681+adibarra@users.noreply.github.com>
2023-02-23 17:45:21 -0600

    feat: display resume

--- pages/resume.md
+++ pages/resume.md
@@ -3,4 +3,5 @@ title: Resume | Alec Ibarra
 display: Resume
 plum: true
 ---
-<span>Download my resume as a PDF <a href="assets/pdfs/Resume - Alec Ibarra.pdf" target="_blank">here</a>.</span>
+
+<ResumeEmbed />
---
+++ src/components/ResumeEmbed.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="absolute ml--8 text-xl"
+    style="transform: rotate(-90deg)"
+  >
+    <a
+      href="assets/pdfs/Resume - Alec Ibarra.pdf"
+      target="_blank"
+      rel="noopener"
+    >
+      <div style="transform: rotate(90deg)" i-carbon-document-download />
+    </a>
+  </div>
+  <img src="assets/images/Resume.png">
+</template>
