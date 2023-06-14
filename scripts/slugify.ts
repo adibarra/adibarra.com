f368d6dc32466ac807b6956dde0aadca109a933b
adibarra <93070681+adibarra@users.noreply.github.com>
2023-06-14 01:51:04 -0500

    feat: update slugify

--- scripts/slugify.ts
+++ scripts/slugify.ts
@@ -1,24 +1,22 @@
-// string.js slugify drops non ascii chars so we have to
-// use a custom implementation here
-import { remove } from 'diacritics'
+const rAccents = /[\u0300-\u036F]/g
 // eslint-disable-next-line no-control-regex
 const rControl = /[\u0000-\u001F]/g
 const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'<>,.?/]+/g
 
 export const slugify = (str: string): string => {
-  return (
-    remove(str)
-      // Remove control characters
-      .replace(rControl, '')
-      // Replace special characters
-      .replace(rSpecial, '-')
-      // Remove continuos separators
-      .replace(/-{2,}/g, '-')
-      // Remove prefixing and trailing separtors
-      .replace(/^-+|-+$/g, '')
-      // ensure it doesn't start with a number (#121)
-      .replace(/^(\d)/, '_$1')
-      // lowercase
-      .toLowerCase()
-  )
+  return str.normalize('NFKD')
+  // remove accents
+  .replace(rAccents, '')
+  // remove control characters
+  .replace(rControl, '')
+  // replace special characters
+  .replace(rSpecial, '-')
+  // remove duplicate separators
+  .replace(/\-{2,}/g, '-')
+  // remove prefixed and trailing separators
+  .replace(/^\-+|\-+$/g, '')
+  // doesn't start with a number
+  .replace(/^(\d)/, '_$1')
+  // lowercase
+  .toLowerCase()
 }
