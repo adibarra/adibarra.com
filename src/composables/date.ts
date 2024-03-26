c60f7cc8eec64c29f1ad5aba5523d3af6e9b7049
adibarra <93070681+adibarra@users.noreply.github.com>
2024-03-25 19:46:50 -0500

    use composables

--- src/composables/date.ts
+++ src/composables/date.ts
@@ -1,14 +1,5 @@
 import dayjs from 'dayjs'
 
-export const isDark = useDark({
-  initialValue: 'dark',
-  storageKey: 'color-scheme',
-})
-
-export function toggleDark() {
-  isDark.value = !isDark.value
-}
-
 export function formatDate(d: string | Date) {
   const date = dayjs(d)
   if (date.year() === dayjs().year())
