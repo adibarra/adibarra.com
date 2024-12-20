6481960e7c9d26b07f55bab44087e78d62b60fc1
adibarra <93070681+adibarra@users.noreply.github.com>
2024-12-19 20:51:50 -0600

    fix: broken email link

--- src/components/EmailLink.vue
+++ src/components/EmailLink.vue
@@ -8,7 +8,7 @@ defineProps<{
   <span relative>
     <div i-carbon:email absolute top-0.5 text--c-text />
     <a
-      :href="`mailto://${to}`"
+      :href="`mailto:${to}`"
       ml-5.5
     >
       {{ to }}
