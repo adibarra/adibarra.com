7257968bee7f44ebf6660343352e9e69ad88ace7
adibarra <93070681+adibarra@users.noreply.github.com>
2024-12-25 14:00:31 -0600

    fix: use correct post name

--- src/components/ListPosts.vue
+++ src/components/ListPosts.vue
@@ -8,7 +8,7 @@ const posts: Post[] = router.getRoutes()
   .sort((a, b) => +new Date(b.meta.frontmatter.date) - +new Date(a.meta.frontmatter.date))
   .map(i => ({
     path: i.path,
-    title: i.meta.frontmatter.title,
+    display: i.meta.frontmatter.display || i.meta.frontmatter.title,
     date: i.meta.frontmatter.date,
     duration: i.meta.frontmatter.duration,
     upcoming: i.meta.frontmatter.upcoming,
@@ -43,7 +43,7 @@ const posts: Post[] = router.getRoutes()
             >
               upcoming
             </span>
-            <span align-middle>{{ route.title }}</span>
+            <span align-middle>{{ route.display }}</span>
           </div>
 
           <div text-sm op-50>
